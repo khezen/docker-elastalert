1bbc763321d3674c8851bc4ed876b5ab4b8dff9a
Guillaume <guillaume@localhost.localdomain>
2017-01-13 15:00:05 +0100

    switch back to master

--- src/setup/elastalert.sh
+++ src/setup/elastalert.sh
@@ -10,7 +10,6 @@ pip install --upgrade six
 #elastalert
 git clone https://github.com/Yelp/elastalert.git /elastalert
 cd /elastalert
-git checkout support_es5
 python setup.py install
 pip install -r requirements.txt
 cp -f config.yaml.example /.backup/elastalert.yml
